317f9300a48680fb90f6bb2ce3eaa7e15570a981
0xyshv <writetoyshv@gmail.com>
2024-11-29 09:07:36 +0530

    feat: add connect button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -15,7 +15,7 @@ export const ConnectButton = () => {
   // const compactAddressWagmi = compactHash(wagmiAccount.address || '')
 
   return (
-    <div className="column">
+    <div className="column z-10">
       {/* <span className="text-black">useAppKitAccount: {compactAddress}</span>
       <span className="text-black">useAccount (wagmi): {compactAddressWagmi}</span> */}
       <appkit-button />
