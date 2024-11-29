d954f0e18bfa117b68033ba5720c40850f0faef1
0xyshv <writetoyshv@gmail.com>
2024-11-29 09:06:28 +0530

    chore: sync

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,7 +3,6 @@
 import { wagmiAdapter, projectId, networks } from '@/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createAppKit } from '@reown/appkit/react'
-import { sepolia } from '@reown/appkit/networks'
 import React, { type ReactNode } from 'react'
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
 
@@ -27,7 +26,6 @@ export const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
   networks,
-  defaultNetwork: sepolia,
   metadata: metadata,
   themeMode: 'light',
   features: {
@@ -46,57 +44,3 @@ function ContextProvider({ children, }: { children: ReactNode; }) {
 }
 
 export default ContextProvider
-
-// "use client";
-
-// import { WagmiProvider, createConfig, http } from "wagmi";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ConnectKitProvider, getDefaultConfig } from "connectkit";
-// import { Chain, sepolia } from "wagmi/chains";
-
-// const bleTestnet: Chain = {
-//   id: 52085143,
-//   name: "Ble Testnet",
-//   rpcUrls: {
-//     default: {
-//       http: ["https://testnet.rpc.ethena.fi"],
-//       webSocket: ["wss://testnet.rpc.ethena.fi"],
-//     },
-//     public: {
-//       http: ["https://testnet.rpc.ethena.fi"],
-//     },
-//   },
-//   blockExplorers: {
-//     default: { name: "Ble Explorer", url: "https://testnet.explorer.ethena.fi" },
-//   },
-//   nativeCurrency: {
-//     name: "Ether",
-//     symbol: "ETH",
-//     decimals: 18,
-//   },
-// };
-
-// const config = createConfig(
-//   getDefaultConfig({
-//     chains: [bleTestnet, sepolia],
-//     transports: {
-//       [bleTestnet.id]: http("https://testnet.rpc.ethena.fi"),
-//     },
-
-//     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-//     appName: "Game of Pots",
-//     appDescription: "A no loss lottery game with savings pool built on sUSDe rewards APY",
-//   })
-// );
-
-// const queryClient = new QueryClient();
-
-// export const Web3Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-//   return (
-//     <WagmiProvider config={config}>
-//       <QueryClientProvider client={queryClient}>
-//         <ConnectKitProvider>{children}</ConnectKitProvider>
-//       </QueryClientProvider>
-//     </WagmiProvider>
-//   );
-// };
