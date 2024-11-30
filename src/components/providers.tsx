e0289bded7dad84a0029665426e0b6d4c1fedb67
0xyshv <writetoyshv@gmail.com>
2024-11-30 11:36:03 +0530

    chore: add ble Testnet

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,15 +1,41 @@
 "use client";
 
 import { WagmiConfig, createConfig } from "wagmi";
-import { sepolia } from "wagmi/chains";
+import { Chain, sepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { ReactNode } from "react";
 
+
+const bleTestnet: Chain = {
+  id: 52085143,
+  name: "Ble Testnet",
+  rpcUrls: {
+    default: {
+      http: ["https://testnet.rpc.ethena.fi"],
+      webSocket: ["wss://testnet.rpc.ethena.fi"],
+    },
+    public: {
+      http: ["https://testnet.rpc.ethena.fi"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Ble Explorer",
+      url: "https://testnet.explorer.ethena.fi",
+    },
+  },
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+};
+
+
 const config = createConfig(
   getDefaultConfig({
-    // Your dApp's chains
-    chains: [sepolia],
+    chains: [sepolia, bleTestnet],
     walletConnectProjectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
     appName: "Game of Pots",
     appDescription: "No loss lottery game with savings pool built on sUSDe rewards APY",
