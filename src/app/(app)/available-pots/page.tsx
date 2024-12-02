38281b45f0ff857f3fa41067f0b37d9f0d6cfe3b
0xyshv <writetoyshv@gmail.com>
2024-12-02 12:24:27 +0530

    chore: add skeletons

--- src/app/(app)/available-pots/page.tsx
+++ src/app/(app)/available-pots/page.tsx
@@ -8,23 +8,21 @@ import { GOP_CONTRACT_ADDRESS } from "@/lib/contracts";
 import { useReadContract } from "wagmi";
 import { GOP_CONTRACT_ABI } from "@/lib/abi";
 import { formatEther } from "viem";
+import PotCardSkeleton from "@/components/PotCardSkeleton";
+import toast from "react-hot-toast";
 
 export default function AvailablePots() {
   const [pots, setPots] = useState<PotCardProps[]>([]);
-  const handleSetPots = () => {
-    setPots(pots);
-  };
-
-  const { data: potData, isLoading } = useReadContract({
+  const { data: availablePotData, isLoading: availablePotLoading, isError: availablePotError } = useReadContract({
     address: GOP_CONTRACT_ADDRESS as `0x${string}`,
     abi: GOP_CONTRACT_ABI,
     functionName: "activePots",
   });
 
   useEffect(() => {
-    console.log("POT DATA", potData);
-    // setPots(potData);
-    const data: [] = potData as [];
+    console.log("POT DATA", availablePotData);
+    // setPots(availablePotData);
+    const data: [] = availablePotData as [];
 
     if (data)
       setPots(
@@ -40,21 +38,40 @@ export default function AvailablePots() {
             pot.status == 0
               ? "active"
               : pot.status == 1
-              ? "earning"
-              : "drawnWinner",
+                ? "earning"
+                : "drawnWinner",
         }))
       );
-  }, [potData]);
+  }, [availablePotData]);
 
-  if (isLoading) {
-    return <div>Loading Available Pots...</div>;
+  useEffect(() => {
+    if (availablePotError) {
+      toast.error("Error fetching available pots");
+    }
+  }, [availablePotError]);
+
+  if (availablePotLoading) {
+    return (
+      <div className="px-4 md:px-20 py-10">
+        <div className="flex justify-between items-center mb-8">
+          <div>
+            <h1 className="text-3xl font-bold mb-4 text-gray-800">
+              Available Pots
+            </h1>
+            <p className="text-gray-600">
+              Join an active pot to start earning rewards and participate in the
+              game.
+            </p>
+          </div>
+          <CreatePotModal />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <PotCardSkeleton />
+          <PotCardSkeleton />
+        </div>
+      </div>
+    );
   }
-
-  // useEffect(() => {
-  //   if (error) {
-  //     toast.error("Error creating pot")
-  //   }
-  // }, [error]);
 
   return (
     <div className="px-4 md:px-20 py-10">
@@ -69,24 +86,29 @@ export default function AvailablePots() {
           </p>
         </div>
         <CreatePotModal />
-        <button onClick={handleSetPots}></button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {pots.map((pot) => (
-          <PotCard
-            key={pot.id}
-            id={pot.id}
-            amount={pot.amount}
-            apy={pot.apy}
-            usdeDeposits={pot.usdeDeposits}
-            maturityPeriod={pot.maturityPeriod}
-            participants={pot.participants}
-            maxParticipants={pot.maxParticipants}
-            status={pot.status}
-          />
-        ))}
-      </div>
+      {pots.length === 0 ? (
+        <div className="flex justify-center items-center pt-28">
+          <p className="text-xl text-gray-600 font-semibold">❌ No pots have been created yet.</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {pots.map((pot) => (
+            <PotCard
+              key={pot.id}
+              id={pot.id}
+              amount={pot.amount}
+              apy={pot.apy}
+              usdeDeposits={pot.usdeDeposits}
+              maturityPeriod={pot.maturityPeriod}
+              participants={pot.participants}
+              maxParticipants={pot.maxParticipants}
+              status={pot.status}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/app/(app)/my-pots/page.tsx
+++ src/app/(app)/my-pots/page.tsx
@@ -7,12 +7,14 @@ import { GOP_CONTRACT_ABI } from "@/lib/abi";
 import { useEffect, useState } from "react";
 import { formatEther } from "viem";
 import { MyPot } from "@/components/MyPot";
+import toast from "react-hot-toast";
+import PotCardSkeleton from "@/components/PotCardSkeleton";
 
 export default function MyPots() {
   const { address } = useAccount();
   const [pots, setPots] = useState<PotCardProps[]>([]);
 
-  const { data: potData, isLoading } = useReadContract({
+  const { data: myPotData, isLoading: myPotLoading, isError: myPotError } = useReadContract({
     address: GOP_CONTRACT_ADDRESS as `0x${string}`,
     abi: GOP_CONTRACT_ABI,
     functionName: "myPots",
@@ -20,9 +22,9 @@ export default function MyPots() {
   });
 
   useEffect(() => {
-    console.log("POT DATA", potData);
+    console.log("POT DATA", myPotData);
 
-    const data: [] = potData as [];
+    const data: [] = myPotData as [];
 
     if (data)
       setPots(
@@ -39,14 +41,33 @@ export default function MyPots() {
             pot.status == 0
               ? "active"
               : pot.status == 1
-              ? "earning"
-              : "drawnWinner",
+                ? "earning"
+                : "drawnWinner",
         }))
       );
-  }, [potData]);
+  }, [myPotData]);
 
-  if (isLoading) {
-    return <div>Loading Available Pots...</div>;
+  useEffect(() => {
+    if (myPotError) {
+      toast.error("Error fetching my pots");
+    }
+  }, [myPotError]);
+
+  if (myPotLoading) {
+    return (
+      <div className="px-4 md:px-20 py-10">
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold mb-4 text-gray-800">My Pots</h1>
+          <p className="text-gray-600">
+            View your active pots and their current status.
+          </p>
+        </div>
+        <div className="space-y-6 mx-auto">
+          <PotCardSkeleton />
+          <PotCardSkeleton />
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -56,24 +77,29 @@ export default function MyPots() {
         <p className="text-gray-600">
           View your active pots and their current status.
         </p>
-        {/* <button onClick={handleSetPots}>abs</button> */}
       </div>
 
-      <div className="space-y-6 mx-auto">
-        {pots.map((pot) => (
-          <MyPot
-            key={pot.id}
-            id={pot.id}
-            amount={pot.amount}
-            apy={pot.apy}
-            usdeDeposits={pot.usdeDeposits}
-            maturityPeriod={pot.maturityPeriod}
-            participants={pot.participants}
-            maxParticipants={pot.maxParticipants}
-            status={pot.status}
-          />
-        ))}
-      </div>
+      {pots.length === 0 ? (
+        <div className="flex justify-center items-center pt-24">
+          <p className="text-xl text-gray-500 font-semibold">❌ No pots have been joined yet.</p>
+        </div>
+      ) : (
+        <div className="space-y-6 mx-auto">
+          {pots.map((pot) => (
+            <MyPot
+              key={pot.id}
+              id={pot.id}
+              amount={pot.amount}
+              apy={pot.apy}
+              usdeDeposits={pot.usdeDeposits}
+              maturityPeriod={pot.maturityPeriod}
+              participants={pot.participants}
+              maxParticipants={pot.maxParticipants}
+              status={pot.status}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
